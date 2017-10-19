54790151de065b8d2b5c4f75ca46f8a64bcaa9d6
UlionTse <shinalone@outlook.com>
2017-10-19 18:38:31 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6"
     ],
-    keywords=['translate','translate_api'],
+    keywords=['requests','crawl_requests'],
     install_requires=[
         'requests>=2.9.1'
     ],
